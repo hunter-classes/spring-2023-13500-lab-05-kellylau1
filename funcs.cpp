37d5b8eb97ddd52a07f52f1aa4e3e6a9115ea5fa
kellylau <kellylaup@gmail.com>
2023-03-01 15:20:45 -0500

    delete main

--- funcs.cpp
+++ funcs.cpp
@@ -71,27 +71,3 @@ return -1;
 
 
 
-int main() {
-    // if (isDivisibleBy(100, 25)) {
-    //     std::cout << "True" << " \n"; 
-    // } else {
-    //     std::cout << "False" << " \n"; 
-    // }
-    // if (isDivisibleBy(35, 17)) {
-    //     std::cout << "True" << " \n"; 
-    // } else {
-    //     std::cout << "False" << " \n"; 
-    // }
-
-    // std::cout << isPrime(37) << '\n'; 
-    // std::cout << isPrime(14) << '\n'; 
-    
-    // std::cout << nextPrime(14) << '\n'; 
-
-    // std::cout << countPrimes(100,1000) << '\n';
-
-    // std::cout << isTwinPrime(17) << '\n';
-    // std::cout << nextTwinPrime(7) << '\n';
-    std::cout << largestTwinPrime(20,28) << '\n';
-return 0; 
-}
